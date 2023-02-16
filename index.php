c0fb74656b98fbd555e810631cf04893c1d0be7a
kiryl-f <filippovickirill81@gmail.com>
2023-02-16 15:14:57 +0300

    -

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once 'create_db.php';
 require_once 'person.php';
 require_once 'person_list.php';
 
-$person = new Person(3, "Name", "Surname","01-01-1970",1,"Ciry");
+/*$person = new Person(3, "Name", "Surname","01-01-1970",1,"Ciry");
 $person->saveToDB();
 $person_list = new PersonList([2,3]);
-$person_list->removePeople();;
+$person_list->removePeople();;*/
